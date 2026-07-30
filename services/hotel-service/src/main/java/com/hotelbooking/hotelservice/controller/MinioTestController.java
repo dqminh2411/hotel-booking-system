2aeedd223fa82699e4a0b4e8b06045d5d4d7504b
Minh <doanminhyb04@gmail.com>
2026-07-30 10:12:29 +0700

    fix(hotel-service): cấu hình oauth2 resource server cho hotel-service, untrack /minio/data folder

--- services/hotel-service/src/main/java/com/hotelbooking/hotelservice/controller/MinioTestController.java
+++ services/hotel-service/src/main/java/com/hotelbooking/hotelservice/controller/MinioTestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("api/test/minio")
+@RequestMapping("api/minio")
 @RequiredArgsConstructor
 public class MinioTestController {
 
